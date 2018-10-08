5df4f4e9c3756f75ac22ff0ccba3d4590f05de67
davesantos <info@davesantos.com>
2018-10-07 18:59:32 -0700

    Add normalize.css, add html-tidy

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,84 +1,82 @@
-'use strict';
+"use strict";
 
-const gulp = require('gulp');
-const browserSync = require('browser-sync');
-const cleanCSS = require('gulp-clean-css');
-const exec = require('child_process').exec
-const prettify = require('gulp-prettify');
-const rmEmptyLines = require('gulp-remove-empty-lines');
-const sass = require('gulp-sass');
-const uglify = require('gulp-uglify');
+import gulp from 'gulp';
+import sass from 'gulp-sass';
+import browserSync from 'browser-sync';
+import cleanCSS from 'gulp-clean-css';
+import htmltidy from 'gulp-htmltidy';
+import rmEmptyLines from 'gulp-remove-empty-lines';
+import uglify from 'gulp-uglify';
+import child_process from 'child_process';
 
+const exec = child_process.exec;
 const paths = {
   build: '_site',
   css: 'css',
-  sass: ['css'],
+  sass: ['_sass'],
   scripts: ['js']
 };
 
 const sassFiles = [
-  'css/**/*.{css,scss,sass}',
+  'css/**/*',
   '_sass/**/*'
 ]
 
 const jsFiles = [
   'js/**/*.js'
-];
+]
 
 const jekyllFiles = [
   '*.{html,yml,md}',
-  '_posts/**/*',
-  '_layouts/**/*',
-  '_includes/**/*'
+  '_posts/*.{markdown,md}',
+  '_layouts/**/*.html',
+  '_includes/**/*.html'
 ];
 
-function errorHandler(error) {
+const messages = {
+  jekyllBuild: '<span style="color: grey">Running:</span> $ jekyll build'
+};
+
+const errorHandler = error => {
   console.error(String(error));
   this.emit('end');
   browserSync.notify('Error');
 }
 
-gulp.task('jekyll-build', function(cb) {
-  exec('bundle exec jekyll build', function(err, stdout, stderr) {
+gulp.task('jekyll-build', done => {
+  exec('bundle exec jekyll build', (err, stdout, stderr) => {
     console.log(stdout);
     console.log(stderr);
-    cb(err);
+    done(err);
   });
 });
 
-gulp.task('jekyll-rebuild', gulp.series('jekyll-build', function(done) {
-  browserSync.reload(), done();
-}));
-
-gulp.task('js', function() {
-  return gulp.src(paths.scripts + '/**/*.js')
+gulp.task('js', () => {
+  return gulp.src(jsFiles)
     .pipe(gulp.dest(paths.build + '/' + paths.scripts))
     .pipe(browserSync.reload({
       stream: true
     }));
 });
 
-gulp.task('sass', gulp.series('jekyll-build', function() {
+gulp.task('sass', gulp.series('jekyll-build', () => {
   return gulp.src(paths.sass + '/*.{sass,scss}')
     .pipe(sass({
        includePaths: ['node_modules']
     }).on('error', errorHandler))
     .pipe(gulp.dest(paths.build + '/' + paths.css))
-    .pipe(gulp.dest(paths.css))
 }));
 
-gulp.task('prettify', gulp.series('jekyll-build', function() {
+gulp.task('htmltidy', gulp.series('jekyll-build', () => {
   return gulp.src([paths.build + '/**/*.html'])
-    .pipe(prettify({
-      indent_inner_html: true,
-      indent_with_tabs: false,
-      indent_size: 2
-    }))
+    .pipe(htmltidy({doctype: 'html5',
+      hideComments: true,
+      indent: true}))
     .pipe(rmEmptyLines())
     .pipe(gulp.dest(paths.build));
 }));
 
-gulp.task('minify', function() {
+gulp.task('minify', () => {
   return gulp.src([paths.build + '/' + paths.css + '/*.css'])
     .pipe(
       cleanCSS({
@@ -91,23 +89,25 @@ gulp.task('minify', function() {
     .pipe(gulp.dest(paths.build + '/' + paths.css))
 });
 
-gulp.task('serve', function(done) {
+gulp.task('serve', gulp.series(gulp.parallel('js', 'sass'), done => {
 
- browserSync.init({
-   server: {
-     baseDir: paths.build
-   }
- });
+  browserSync.init({
+    server: {
+      baseDir: paths.build,
+    },
+    notify: false
+  });
 
-  gulp.watch(jekyllFiles).on('all', gulp.series('jekyll-build'));
+  gulp.watch(jekyllFiles).on('all', gulp.series('sass'));
   gulp.watch(sassFiles).on('change', gulp.series('sass'));
   gulp.watch(jsFiles).on('change', gulp.series('js'));
   gulp.watch(paths.build).on('all', browserSync.reload);
   return console.log('Initializing Server...'), done();
-});
 
-gulp.task('travis', gulp.series(gulp.parallel('sass', 'js', 'prettify', 'minify'), function(done) {
-  return console.log('complete'), done();
+}));
+
+gulp.task('travis', gulp.series(gulp.parallel('sass', 'js', 'htmltidy', 'minify'), done => {
+  console.log('complete'), done();
 }));
 
 gulp.task('default', gulp.series('serve'));
